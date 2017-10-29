d1ac6746c36fb4e27f09c3a15fdc9789d5f6e98a
geroge-gao <1375641052@qq.com>
2017-10-29 15:36:13 +0800

    fix some errors

--- CART/cart.py
+++ CART/cart.py
@@ -269,29 +269,6 @@ def createForeCast(tree,testData,modelEval = regTreeEval):
         yHat[i,0]=treeForeCast(tree,mat(testData[i]),modelEval)
     return yHat
 
-def regTreeEval(model, inDat):
-    return float(model)
-
-def modelTreeEval(model, inDat):
-    n = shape(inDat)[1]
-    X = mat(ones((1, n + 1)))
-    X[:, 1:n + 1] = inDat
-    return float(X * model)
-
-def treeForeCast(tree, inData, modelEval=regTreeEval):
-    if not isTree(tree): return modelEval(tree, inData)
-    if inData[tree['spInd']] > tree['spVal']:
-        if isTree(tree['left']):
-            return treeForeCast(tree['left'], inData, modelEval)
-        else:
-            return modelEval(tree['left'], inData)
-    else:
-        if isTree(tree['right']):
-            return treeForeCast(tree['right'], inData, modelEval)
-        else:
-            return modelEval(tree['right'], inData)
-
-
 def createForeCast(tree, testData, modelEval=regTreeEval):
     m = len(testData)
     yHat = mat(zeros((m, 1)))
